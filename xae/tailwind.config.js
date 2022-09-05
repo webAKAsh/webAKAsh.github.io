e6340fd6eed785e9bbaa04dbd3a0b179cd774fc3
metaltank-blast <codingmetaltank@gmail.com>
2022-09-06 00:33:32 +0530

    xae

---
+++ xae/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundPosition:{
+      pyramid:['90% -75px']
+    },
+    fontFamily:{
+      'poppins':['Orbitron', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
